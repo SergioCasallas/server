bc38459607a3208a7ff6fae95512ddefe94e5326
SergioCasallas <sergiocasallas01@gmail.com>
2022-02-11 21:51:20 -0500

    Querys,Relecciones And Add Query RecoleccionesDatosPdf

--- src/db/db.js
+++ src/db/db.js
@@ -7,6 +7,8 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  timezone: "utc",
+  dateStrings: true,
 });
 
 connection.connect(async (error) => {
--- src/queries/Recolecciones/Recolecciones_queries.js
+++ src/queries/Recolecciones/Recolecciones_queries.js
@@ -10,46 +10,51 @@ exports.getRecoleccionesResidues = (
     UUIDSede.length > 0 &&
     (fechaInicial, fechaFinal)
   ) {
-    return `SELECT UUID_Sede,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in (${UUIDSede.map(
+    return `SELECT UUID_Sede,primary_secondary,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in (${UUIDSede.map(
       (item) => `"${item.UUID}"`
-    )}) AND (client_signature_timestamp BETWEEN "${fechaInicial}" AND "${fechaFinal}")  AND residue = "${residue}" and primary_secondary = 'Primary'  `;
+    )}) AND (client_signature_timestamp BETWEEN "${fechaInicial}" AND "${fechaFinal}")  AND residue = "${residue}"  ORDER BY created_date DESC `;
   } else if (residue && (fechaInicial, fechaFinal) && UUIDSede) {
-    return `SELECT UUID_Sede,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in ("${UUIDSede}") AND (client_signature_timestamp BETWEEN "${fechaInicial}" AND "${fechaFinal}")  AND residue = "${residue}"  and primary_secondary = 'Primary'  `;
+    return `SELECT UUID_Sede,primary_secondary,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in ("${UUIDSede}") AND (client_signature_timestamp BETWEEN "${fechaInicial}" AND "${fechaFinal}")  AND residue = "${residue}"   ORDER BY created_date DESC `;
   } else if (residue && UUIDSede.length > 0 && typeof UUIDSede !== "string") {
-    return `SELECT UUID_Sede,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in (${UUIDSede.map(
+    return `SELECT UUID_Sede,primary_secondary,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in (${UUIDSede.map(
       (item) => `"${item.UUID}"`
-    )})   AND residue = "${residue}"  and primary_secondary = 'Primary'  `;
+    )})   AND residue = "${residue}"   ORDER BY created_date DESC `;
   } else {
-    return `SELECT UUID_Sede,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in ("${UUIDSede}") AND residue = "${residue}"  and primary_secondary = 'Primary'  `;
+    return `SELECT UUID_Sede,primary_secondary,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in ("${UUIDSede}") AND residue = "${residue}"   ORDER BY created_date DESC `;
   }
 };
+
 
 exports.getRecolecciones = (UUIDSede) => {
   if (typeof UUIDSede !== "string") {
-    return `SELECT UUID_Sede,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in (${UUIDSede.map(
+    return `SELECT UUID_Sede,primary_secondary,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in (${UUIDSede.map(
       (item) => `"${item.UUID}"`
-    )})  and primary_secondary = 'Primary'  `;
+    )})   ORDER BY created_date DESC  `;
   } else {
-    return `SELECT UUID_Sede,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in ("${UUIDSede}")  and primary_secondary = 'Primary'  `;
+    return `SELECT UUID_Sede,primary_secondary,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in ("${UUIDSede}")    ORDER BY created_date DESC`;
   }
 };
 
+
 exports.getRecoleccionesFechas = (UUIDSede, fechaInicial, fechaFinal) => {
   if (typeof UUIDSede !== "string") {
-    return `SELECT UUID_Sede,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in (${UUIDSede.map(
+    return `SELECT UUID_Sede,primary_secondary,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in (${UUIDSede.map(
       (item) => `"${item.UUID}"`
-    )}) AND (client_signature_timestamp BETWEEN "${fechaInicial}" AND "${fechaFinal}")  and primary_secondary = 'Primary'  `;
+    )}) AND (client_signature_timestamp BETWEEN "${fechaInicial}" AND "${fechaFinal}")   ORDER BY created_date DESC`;
   } else {
-    return `SELECT UUID_Sede,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in ("${UUIDSede}") AND (client_signature_timestamp BETWEEN "${fechaInicial}" AND "${fechaFinal}")  and primary_secondary = 'Primary'  `;
+    return `SELECT UUID_Sede,primary_secondary,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where UUID_Sede in ("${UUIDSede}") AND (client_signature_timestamp BETWEEN "${fechaInicial}" AND "${fechaFinal}")   ORDER BY created_date DESC `;
   }
 };
 
 exports.getRecoleccionesWorkPlan = (numeroReporte, UUIDSede) => {
   if (typeof UUIDSede !== "string") {
-    return `SELECT UUID_Sede,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where work_plan_no='${numeroReporte}' and UUID_Sede in (${UUIDSede.map(
+    return `SELECT UUID_Sede,primary_secondary,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where work_plan_no='${numeroReporte}' and UUID_Sede in (${UUIDSede.map(
       (item) => `"${item.UUID}"`
-    )})  and primary_secondary = 'Primary'  `;
+    )})   ORDER BY created_date DESC `;
   } else {
-    return `SELECT UUID_Sede,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where  work_plan_no='${numeroReporte}' and UUID_Sede in ("${UUIDSede}")  and primary_secondary = 'Primary'  `;
+    return `SELECT UUID_Sede,primary_secondary,work_plan_detail_id,contact_name,contact_cc,residue_physical_state,work_plan_no, company_address, created_date, residue, confirmed_weight, confirmed_quantity  FROM rp_track_and_trace_framework.tbl_rp_tx_work_plan_details where  work_plan_no='${numeroReporte}' and UUID_Sede in ("${UUIDSede}") ORDER BY created_date DESC `;
   }
 };
+
+
+// confirmed_weight
